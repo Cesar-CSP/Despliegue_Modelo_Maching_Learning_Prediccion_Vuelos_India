040074d95fd50eeae5b20a97c341f3ab4785e8c3
Cesar-CSP <cesar.sanchez.parra@gmail.com>
2026-03-30 19:38:37 +0200

    Update app_model_eco.py

--- app_model_eco.py
+++ app_model_eco.py
@@ -18,7 +18,10 @@ def hello():
     <h1>CLA Flight Intelligence</h1>
     <p>Bienvenido a nuestra API del modelo de predicción de vuelos de la India de la clase economy</p>
     """
-
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("landing_page.html")
+    
 # Enruta la funcion al endpoint /api/v1/predict
 @app.route('/api/v1/predict', methods=['GET'])
 def predict():
